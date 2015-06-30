9a6019ead7c3378de80b944260eba0d5921eb914
linnealovespie <linnealovespie@gmail.com>
2015-06-29 22:27:39 -0500

    Set up the blog/pagination, *just* need to add in the entries :P

--- js/baseAnimations.js
+++ js/baseAnimations.js
@@ -9,3 +9,20 @@ $( "#calendar-btn" ).click(function() {
         $( ".calendar-div" ).show( "slide");
 }); 
 
+var items = $('post').length;
+var itemsPerPage = 4;
+$('.post').hide();
+$('.post').slice(0, itemsPerPage).show();
+$(function() {
+    $('.news-pagination').pagination({
+        items: items,
+        itemsOnPage: itemsPerPage,
+        onPageClick: function(pageNumber) {
+            var showFrom = itemsPerPage * (pageNumber - 1);
+            var showTo = showFrom
+            itemsPerPage;
+            $('.post').hide();
+            $('.post').slice(showFrom, showTo).show();
+        }
+    });
+});
