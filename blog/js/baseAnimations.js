36999a61d4c90eb30efe083dfdb11c57d121c625
Linnea May <linnealovespie@gmail.com>
2017-03-06 21:18:02 -0600

    jekyll is cancer

---
+++ blog/js/baseAnimations.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    $(".dropdown-button").dropdown();
+    $('.slider').slider('start');
+});
+
+$(".button-collapse").sideNav();
+var height = $(window).height();
+$('.bg-img').css('height', height);
+
+
+/*$( "#calendar-btn" ).click(function() {
+        $( ".calendar-div" ).show( "slide");
+        $(".events-wrapper").css('display: none');
+}); */
+
+/*$(".play-btn").click(function(){
+    var source = $("#first-slide").src;
+    $('.slides li').hide();
+    //$(".slides").css('background-image', "url('img/blog/2015/regionals2015-0.jpg')");
+    //style.backgroundImage = "url('img/blog/2015/regionals2015-0.jpg')";
+    $(".slides li").first().show();
+    $(".play-btn").hide();
+    $(".pause-btn i ").show();
+});
+
+$(".pause-btn").click(function(){
+    $('.slides').show();
+    $(".pause-btn i ").hide();
+    $(".play-btn").show();
+});*/
